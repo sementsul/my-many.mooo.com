3b9c45fb058e65dc10d388b6a4dc9e94033ed69a
Sementsul <45505876+sementsul@users.noreply.github.com>
2026-09-09 23:51:03 +0300

    fix: remove Yell.ru link fully

--- build.py
+++ build.py
@@ -405,7 +405,7 @@ def foot(lang):
             '<a href="https://t.me/ratescout_kurs" target="_blank" rel="noopener me">Telegram</a> · '
             '<a href="https://ok.ru/group/70000057243663" target="_blank" rel="noopener me">Одноклассники</a> · '
             '<a href="https://mastodon.social/@ratescout_ru" target="_blank" rel="noopener me">Mastodon</a> · '
-            '<a href="https://www.yell.ru/moscow/com/ratescout-ru_14524615/" target="_blank" rel="noopener me">Yell.ru</a> · '
+            ' · '
             f'<a href="{rs(lang, "/raskrytie/")}">Раскрытие</a> · '
             f'<a href="{rs(lang, "/politika/")}">Политика конфиденциальности</a>')
         fine = ("18+. Информация носит справочный характер, не является рекламой, офертой или финансовой "
